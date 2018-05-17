7b143a72a7da38019907aa273f2204292fea41b3
breadcat <peter@minskio.co.uk>
2018-05-17 11:08:58 +0100

    Add absolute logger

---
+++ absolute-60s/absolute.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while true
+do
+  pageuri=https://absoluteradio.co.uk/60s/music/ # webpage variable
+  outfile=/pub/absolute-60s.txt # output file
+  pagesource=$(wget "$pageuri" -qO-) # grab base file as variable
+  artists=$(printf "$pagesource" | grep song-artist\" | sed 's/^.*>\([^<]*\)<.*$/\1/') # artists variable
+  titles=$(printf "$pagesource" | grep song-title | cut -d\> -f3 | cut -d\< -f1) # titles variable
+  paste <(printf "$artists") <(printf "$titles") | sed -e 's/\t/ - /g' >> "$outfile" # merge line by line
+  sed -i -e 's/^The /\t/' "$outfile" # replace articles
+  sort -b -u -o "$outfile" "$outfile" # sort and de-dupe output
+  sed -i -e 's/^\t/The /' "$outfile" # revert articles
+  chown 1000:1000 "$outfile" # set owner to non-root
+  printf "Script run on $(date)\n"
+  sleep 900 # sleep for 15 miutes
+done
