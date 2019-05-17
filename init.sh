2699e78d3a34e6c841d215079ae87337451f9db6
breadcat <peter.gill@chillitelecom.co.uk>
2019-05-17 11:55:43 +0100

    Remove static env file, incorporate into init script

--- init.sh
+++ init.sh
@@ -1,7 +1,49 @@
 #!/bin/bash
 
-echo Removing old volumes
-for i in volume image system; do docker $i prune -f; done
+# delete env file
+if [[ -f ".env" ]]; then echo Deleting existing env file && rm ".env"; fi
 
-echo Running docker-compose
+# variables and functions
+function rclone_remote() { grep "$1" "$HOME/.config/rclone/rclone.conf" | sed 1q | tr -d \[\]; }
+function domain_find() { grep domain "$HOME/config/traefik/traefik.toml" | cut -f2 -d\"; }
+
+# password management
+PASS_FILE="password.txt"
+if ! [[ -f "$PASS_FILE" ]]; then head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16 > "$PASS_FILE"; fi
+
+# write env file
+echo Writing env file
+cat << EOF > .env
+NAME=$(whoami)
+PASS=$(cat $PASS_FILE)
+DOMAIN=$(domain_find)
+
+PUID=$(id -u)
+PGID=$(id | cut -f2 -d= | cut -f1 -d\()
+TZ=$(cat /etc/timezone)
+
+CONFDIR=$HOME/config
+DOWNDIR=$HOME/downloads
+POOLDIR=$HOME/media
+SAVEDIR=$HOME/saves
+SYNCDIR=$HOME/vault
+WORKDIR=$HOME/paperwork
+
+RCLONE_REMOTE_MEDIA=$(rclone_remote media)
+RCLONE_REMOTE_SAVES=$(rclone_remote saves)
+RCLONE_REMOTE_WORK=$(rclone_remote work)
+EOF
+
+# clean up existing stuff
+echo Cleaning up existing docker files
+for i in volume image system network; do docker "$i" prune -f; done
+
+# make network, if not existing
+if ! echo "$(docker network ls)" | grep -q "proxy"; then echo Creating docker network && docker network create proxy; fi
+
+# start containers
+echo Starting docker containers
 docker-compose up -d --remove-orphans
+
+# remove env file
+if [[ -f ".env" ]]; then echo Removing env file && rm ".env"; fi
