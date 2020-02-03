7d00d3439a6dba58cc56776683326ce858eb1079
Peter <peter@minskio.co.uk>
2020-02-03 11:57:50 +0000

    Clean more junk

--- server.sh
+++ server.sh
@@ -324,6 +324,7 @@ function func_junk_clean {
 	find "$working_directory" -type f -name "*sample*" -delete
 	find "$working_directory" -type f -name "AhaShare*" -delete
 	find "$working_directory" -type d -iname 'sample' -exec rm -r {} +
+	find "$working_directory" -type d -iname 'screens' -exec rm -r {} +
 	find "$working_directory" -type d -empty -delete
 	}
 function func_rclone_mount {
